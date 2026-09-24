51d533db0698c958833c89e90bcdb39db0f25e88
thisissamiam <sam.kibiger@hak12.org>
2026-09-24 11:16:51 -0400

    fix

--- main.py
+++ main.py
@@ -2,43 +2,48 @@ from opensimplex import OpenSimplex
 import random
 import time
 import os
+
 world = []
 
 seed = random.randint(0, 100000000)
 print("Seed:", seed)
+
 l = 0
 gen = OpenSimplex(seed=seed)
+
+
+def loop():
+    global l
+    global world
+
+    width = 100
+    height = 30
+
+    world = []
+
+    for y in range(height):
+        row = []
+        for x in range(width):
+            row.append(' ')
+        world.append(row)
+
+    for x in range(width):
+        small = gen.noise2((x + l) / 10.0, 0) * 2
+        medium = gen.noise2((x + l) / 50.0, 0) * 4
+        big = gen.noise2((x + l) / 200.0, 0) * 8
+
+        terrain_height = int(8 + small + medium + big)
+
+        for y in range(terrain_height):
+            world[height - 1 - y][x] = 'D'
+
+
 for i in range(1000):
+    loop()
 
-	def loop():
-	    global l
-	    global world
+    for row in world:
+        print("".join(row))
 
-	    width = 100
-	    height = 30
-
-	    world = []
-
-	    for y in range(height):
-	        row = []
-	        for x in range(width):
-	            row.append(' ')
-	        world.append(row)
-
-	    for x in range(width):
-	        small = gen.noise2((x + l) / 10.0, 0) * 2
-			medium = gen.noise2((x + l) / 50.0, 0) * 4
-			big = gen.noise2((x + l) / 200.0, 0) * 8
-
-			terrain_height = int(8 + small + medium + big)
-
-	        for y in range(terrain_height):
-	            world[height - 1 - y][x] = 'D'
-
-	loop()
-
-	for row in world:
-	    print(row)
-	l += 1
-	time.sleep(1/60)
-	os.system('clear')
+    l += 1
+    time.sleep(1 / 60)
+    os.system("clear")
