ae1acd27820d73f581f76c1a8a7a5ddb8e369f11
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 07:36:05 -0400

    fix

--- code.py
+++ code.py
@@ -6,13 +6,15 @@ world = [] # Create the 2d world, starting as nothing
 seed = random.randint(0, 100000000) # Generate the seed, determines what is generated
 gen = OpenSimplex(seed=seed)
 def gameloop():
-    while True:
-        for y in range(height):
-                row = []
-                for x in range(width):
-                    row.append(' ')
-                world.append(row)
-        game.refresh()
+    # Chunk Settings
+    width = 100
+    height = 30
+    for y in range(height):
+            row = []
+            for x in range(width):
+                row.append(' ')
+            world.append(row)
+    game.refresh()
 
 def main(stdscr):
     global game
