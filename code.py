a1c235a086dc65e53748cf45148c4df9e3e4fe50
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 10:00:13 -0400

    fix

--- code.py
+++ code.py
@@ -7,11 +7,7 @@ gen = OpenSimplex(seed=seed)
 term = blessed.Terminal() # Term is basicly the terminal, allows editing and getting data about the terminal.
 print(term.clear) # Clear the screen before printing
 # Chunk Settings
-print(term.clear)
 print(term.home, end="")
-
-for i in range(30):
-    print("*" * 100)
 # width = 100
 # height = 30
 # chunk = ['*'] * width * height
