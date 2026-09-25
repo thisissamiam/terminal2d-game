5a8096c2532d8ec7a85d92981efc14946ad098bd
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 09:57:34 -0400

    fix

--- code.py
+++ code.py
@@ -5,3 +5,5 @@ world = [] # Create the 2d world, starting as nothing
 seed = random.randint(0, 100000000) # Generate the seed, determines what is generated
 gen = OpenSimplex(seed=seed)
 term = blessed.Terminal() # Term is basicly the terminal, allows editing and getting data about the terminal.
+print(term.clear) # Clear the screen before printing
+# Chunk Settings
