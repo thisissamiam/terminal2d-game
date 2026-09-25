978c8981a50a18509ab6dd2443b29eee018af7b7
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 11:22:43 -0400

    fix

--- code.py
+++ code.py
@@ -10,6 +10,7 @@ print(term.clear) # Clear the screen before printing
 width = 100
 height = 30
 chunk = [' '] * width * height
+lasttopheight = None
 for x in range(width):
     isflat = abs(int(gen.noise2(x / 20.0, 0))) < 0.2
     if isflat:
