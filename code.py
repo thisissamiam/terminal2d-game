7320f98e186fc9a47cc71b9ec8e416b12dd97a9d
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 11:10:31 -0400

    fix

--- code.py
+++ code.py
@@ -12,7 +12,7 @@ height = 30
 chunk = [' '] * width * height
 for x in range(100):
     topheight = 15 - int(gen.noise2(x / 10.0, 0)*5)
-    for y in range(10,topheight):
+    for y in range(topheight,10):
         chunk[y * width + x] = '!'
 
 
