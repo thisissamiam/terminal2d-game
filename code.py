6ea1fb93d8255678b0d2e4c23926aa6ea68b6949
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 09:14:12 -0400

    fix

--- code.py
+++ code.py
@@ -7,9 +7,7 @@ world = [] # Create the 2d world, starting as nothing
 seed = random.randint(0, 100000000) # Generate the seed, determines what is generated
 gen = OpenSimplex(seed=seed)
 term = blessed.Terminal() # Term is basicly the terminal, allows editing and getting data about the terminal.
-for i in range(50):
-    print(f"\033[48;2;255;0;{100+i}m \033[0m", end="")
-print()
+term.does_text_sizing()
 def gameloop():
     # Chunk Settings
     width = 100
