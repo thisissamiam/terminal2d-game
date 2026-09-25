43cbbb3d9f612dee7833dd0d6d92c42198f37da7
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 09:25:20 -0400

    fix

--- code.py
+++ code.py
@@ -1,26 +1,18 @@
-import curses
 import random
 from opensimplex import OpenSimplex
 import blessed
-game = None # Create empty game screen
 world = [] # Create the 2d world, starting as nothing
 seed = random.randint(0, 100000000) # Generate the seed, determines what is generated
 gen = OpenSimplex(seed=seed)
 term = blessed.Terminal() # Term is basicly the terminal, allows editing and getting data about the terminal.
-print(term.does_text_sizing())
 def gameloop():
     # Chunk Settings
     width = 100
     height = 30
-    for y in range(height):
+    for y in range(height): # Make a chunk
             row = []
             for x in range(width):
                 row.append(' ')
             world.append(row)
-    game.refresh()
-
-def main(stdscr):
-    global game
-    game = stdscr
-    gameloop()
-curses.wrapper(main)
+            for row in world:
+                print(term.color_rgb(255, 0, 255)(row))
