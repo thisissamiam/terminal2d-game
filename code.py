c91166026d7cacca451a21753851cbf68875e6cc
thisissamiam <sam.kibiger@hak12.org>
2026-09-25 09:25:34 -0400

    fix

--- code.py
+++ code.py
@@ -5,14 +5,13 @@ world = [] # Create the 2d world, starting as nothing
 seed = random.randint(0, 100000000) # Generate the seed, determines what is generated
 gen = OpenSimplex(seed=seed)
 term = blessed.Terminal() # Term is basicly the terminal, allows editing and getting data about the terminal.
-def gameloop():
-    # Chunk Settings
-    width = 100
-    height = 30
-    for y in range(height): # Make a chunk
-            row = []
-            for x in range(width):
-                row.append(' ')
-            world.append(row)
-            for row in world:
-                print(term.color_rgb(255, 0, 255)(row))
+# Chunk Settings
+width = 100
+height = 30
+for y in range(height): # Make a chunk
+        row = []
+        for x in range(width):
+            row.append(' ')
+        world.append(row)
+        for row in world:
+            print(term.color_rgb(255, 0, 255)(row))
